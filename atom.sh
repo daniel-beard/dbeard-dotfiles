90ea6d1cb6e59fce521396939c4c1d9b40b7a2c0
daniel-beard <daniel.beard0@gmail.com>
2015-12-23 13:14:59 -0800

    Install atom packages

---
+++ atom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ev
+
+# This file installs atom.io packages
+
+apm install hydrogen
+apm install vim-mode
--- bootstrap.sh
+++ bootstrap.sh
@@ -22,3 +22,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Misc utility installs
 ./other_installs.sh
+
+# Install atom.io packages
+./atom.sh
